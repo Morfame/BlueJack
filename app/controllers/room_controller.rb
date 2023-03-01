3d6470005d530e63b538ab11a41481c3e41948dd
Steve Morris <baramorf.com>
2023-03-01 14:28:39 +0000

    Learning Curve. very steep

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,24 @@
+class RoomController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+end
